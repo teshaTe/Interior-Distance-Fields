063932f08ea691842e7290229e51f492df094992
Alexander Tereshin <tereshin.tesha@gmail.com>
2020-07-25 15:28:11 +0100

    ADDED: description of the class + separate function for computing pairwise distances

--- IDF/include/interiorDistanceField.h
+++ IDF/include/interiorDistanceField.h
@@ -1,6 +1,23 @@
 #ifndef H_INTERIOR_DISTANCE_FIELDS_CLASS
 #define H_INTERIOR_DISTANCE_FIELDS_CLASS
 
+/*
+ * This class is based on the following papers:
+ * Rustamov, R.M., Lipman, Y. and Funkhouser, T. (2009), Interior Distance Using Barycentric Coordinates.
+ * Computer Graphics Forum, 28: 1279-1288. doi:10.1111/j.1467-8659.2009.01505.x
+ *
+ * The external library - dmaps is based on the following paper:
+ * Ronald R. Coifman, Stéphane Lafon, Diffusion maps, Applied and Computational Harmonic Analysis,
+ * Volume 21, Issue 1, 2006, Pages 5-30, https://doi.org/10.1016/j.acha.2006.04.006.
+ *
+ * GTS library is used for extracting polygonised surface of the object defined by the function
+ * for its further processing in dmaps to obtain diffusion map.
+ *
+ * libIGL library is used for cleaning the obtained mesh after triangulation/tetrahidralisation
+ * and rendering the computed IDF.
+ *
+ */
+
 #include "dmaps/include/diffusion_map.h"
 #include "dmaps/include/distance_matrix.h"
 
@@ -31,6 +48,8 @@ public:
     void computeIDF_mesh3D(const Eigen::MatrixXd &meshVerts, const Eigen::MatrixXi &meshFaces, const Eigen::Vector3d &srcP, const int eigVecNumber, double kernelBandW);
     void computeIDF_mesh3D(GtsIsoCartesianFunc func, const Eigen::Vector3d &srcP, const Eigen::Vector3i gridRes, double iso,
                            const int eigVecNumber, double kernelBandW);
+    void computeIDF_slice(const Eigen::MatrixXd &meshVerts, const Eigen::MatrixXi &meshFaces, const Eigen::Vector3d &srcP,
+                          const int eigVecNumber, double kernelBandW);
     void computeIDF_slice(GtsIsoCartesianFunc func, const Eigen::Vector3d &srcP, const Eigen::Vector3i gridRes, double iso,
                           const int eigVecNumber, double kernelBandW);
 
@@ -54,11 +73,13 @@ private:
     void computeInteriorDF2D(const Eigen::MatrixXd &surfMeshV, const Eigen::MatrixXd &inVerts, const Eigen::VectorXd &srcP);
     void computeInteriorDF3D(const Eigen::MatrixXd &surfMeshV, const Eigen::MatrixXd &inVerts, const Eigen::MatrixXi faces, const Eigen::VectorXd &srcP);
     void iterateIDF_slice(const Eigen::MatrixXd &surfMeshV, const Eigen::MatrixXi faces, const Eigen::VectorXd &srcP );
+    void autoSelectEigenVectors(Eigen::VectorXf &eigVals_s, Eigen::MatrixXf &eigVecs_s, const float t);
     void update_visualization(igl::opengl::glfw::Viewer &viewer);
     void setColormap(igl::opengl::glfw::Viewer & viewer, int isoNum);
     bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier);
 
-    void findZeroLevelSet2D(const std::vector<float> func, int resX, int resY);
+    Eigen::MatrixXf computePairwiseDist();
+
 private:
     dmaps::matrix_t eigVecs;
     dmaps::vector_t eigVals;
@@ -77,5 +98,5 @@ private:
     double slice_z;
 };
 
-} // namespace hfrep
+} // namespace idf
 #endif
